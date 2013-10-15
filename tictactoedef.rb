0a290222373b41819bcb03f5d724f9b749b109c1
Eleanor Lee <ellie007@ELEANOR-LEEs-MacBook-Pro.local>
2013-10-14 23:40:26 -0500

    added accessor

--- tictactoedef.rb
+++ tictactoedef.rb
@@ -1,10 +1,11 @@
 class TicTacToe
 
   def display_game_board
-  @possible_places = {
-      a1:" ",a2:" ",a3:" ",
-      b1:" ",b2:" ",b3:" ",
-      c1:" ",c2:" ",c3:" "
+    attr_accessor :a1, :a2, :a3, :b1, :b2, :b3, :c1, :c2, :c3
+    @possible_places = {
+     a1: @a1,a2: @a2,a3: @a3,
+     b1: @b1,b2: @b2,b3: @b3,
+     c1: @c1,c2: @c2,c3: @c3}
 
 
       puts "#{@possible_places[:a1]} | #{@possible_places[:a2]} | #{@possible_places[:a3]}"
@@ -14,13 +15,10 @@ class TicTacToe
       puts "#{@possible_places[:c1]} | #{@possible_places[:c2]} | #{@possible_places[:c3]}"
   end
 
-
-
-
    def user_turn
     input=gets.chomp.to_sym
     @possible_places[input] = 's'
-puts display_game_board
+    puts display_game_board
   end
 
 
